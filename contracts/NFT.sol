913c78478308ed077ee343d550da7c01a4e5df5c
Monnnx <itsmeh0295@gmail.com>
2024-11-10 09:21:52 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("cok wey","ckw")
+        ERC721("coklah","ckl")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
